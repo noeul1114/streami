3206e5cd6966972b343e60b3e8eec970c29bb65c
Park HS <parkhyongsok@naver.com>
2020-02-27 20:07:01 +0900

    point_list argument deleted > reference with self.point_list

--- Manager/base.py
+++ Manager/base.py
@@ -13,9 +13,9 @@ class Point:
 class Manager:
     def __init__(self, **kwargs):
         try:
-            point_list = kwargs['list']
+            self.point_list = kwargs['list']
         except:
-            point_list = None
+            self.point_list = None
         self.c = 0              # counter 설정.
 
         self.root = Tk()        # 기본 윈도우 설정
@@ -37,18 +37,19 @@ class Manager:
                                              5 + y * 10 + 7,
                                              fill='grey')
 
-        if point_list:
-            for i in range(len(point_list)):
-                print(point_list[i].x, point_list[i].y)
+        # 입력받은 point_list 가 존재한다면 해당 값들을 초기화.
+        if self.point_list:
+            for i in range(len(self.point_list)):
+                print(self.point_list[i].x, self.point_list[i].y)
                 self.canvas.create_rectangle(
-                    point_list[i].x * 10 + 5,
-                    point_list[i].y * 10 + 5,
-                    point_list[i].x * 10 + 12,
-                    point_list[i].y * 10 + 12,
+                    self.point_list[i].x * 10 + 5,
+                    self.point_list[i].y * 10 + 5,
+                    self.point_list[i].x * 10 + 12,
+                    self.point_list[i].y * 10 + 12,
                     fill="red")
 
-        if point_list:
-            self.tick_with_init(point_list)
+        if self.point_list:
+            self.tick_with_init()
         else:
             self.tick_random()
         self.root.mainloop()
@@ -66,7 +67,7 @@ class Manager:
         else:
             self.canvas.after(1000, self.tick_random)
 
-    def tick_with_init(self, point_list):
+    def tick_with_init(self):
         self.canvas.create_rectangle(5+self.c*10,
                                      5+self.c*10,
                                      5+self.c*10+7,
