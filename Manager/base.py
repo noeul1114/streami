7f149d7b79732f949441b14331990323676caa23
Park HS <parkhyongsok@naver.com>
2020-02-27 20:01:15 +0900

    Point class update
    PLAY_TYPE 2 updated

--- Manager/base.py
+++ Manager/base.py
@@ -4,9 +4,18 @@ from tkinter import *
 width = 80
 height = 40
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 class Manager:
-    def __init__(self):
+    def __init__(self, **kwargs):
+        try:
+            point_list = kwargs['list']
+        except:
+            point_list = None
         self.c = 0              # counter 설정.
 
         self.root = Tk()        # 기본 윈도우 설정
@@ -22,17 +31,30 @@ class Manager:
         # 기본 바탕이 되는 픽셀들 그리기.
         for x in range(width):
             for y in range(height):
-                self.canvas.create_rectangle(5+x*10,
-                                             5+y*10,
-                                             5+x*10+7,
-                                             5+y*10+7,
+                self.canvas.create_rectangle(5 + x * 10,
+                                             5 + y * 10,
+                                             5 + x * 10 + 7,
+                                             5 + y * 10 + 7,
                                              fill='grey')
 
-        self.tick()
+        if point_list:
+            for i in range(len(point_list)):
+                print(point_list[i].x, point_list[i].y)
+                self.canvas.create_rectangle(
+                    point_list[i].x * 10 + 5,
+                    point_list[i].y * 10 + 5,
+                    point_list[i].x * 10 + 12,
+                    point_list[i].y * 10 + 12,
+                    fill="red")
+
+        if point_list:
+            self.tick_with_init(point_list)
+        else:
+            self.tick_random()
         self.root.mainloop()
 
     # mainloop 내에서 작동되는 작업. 1초마다 카운터를 증가시키며, 시간 간격마다 캔버스를 다시 그림.
-    def tick(self):
+    def tick_random(self):
         self.canvas.create_rectangle(5+self.c*10,
                                      5+self.c*10,
                                      5+self.c*10+7,
@@ -42,4 +64,16 @@ class Manager:
         if self.c == 10000:
             print(f"Counter {self.c} reached.")
         else:
-            self.canvas.after(1000, self.tick)
+            self.canvas.after(1000, self.tick_random)
+
+    def tick_with_init(self, point_list):
+        self.canvas.create_rectangle(5+self.c*10,
+                                     5+self.c*10,
+                                     5+self.c*10+7,
+                                     5+self.c*10+7,
+                                     fill='red')
+        self.c += 1
+        if self.c == 10000:
+            print(f"Counter {self.c} reached.")
+        else:
+            self.canvas.after(1000, self.tick_with_init)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import argparse
-import time
 
-from Manager import base
+from Manager.base import Manager, Point
 
 # Optional args 추가
 parser = argparse.ArgumentParser()
@@ -14,25 +13,44 @@ args = parser.parse_args()
 # 기본적으로 입력이 없을 경우 플레이 방법은 1
 PLAY_TYPE = 1
 
+initial_cell_list = []
+NUM_ROWS, NUM_COLS = 80, 40
+
 # Input txt 파일이 정상적으로 열리는지 확인.
 if args.text:
     text_file_dir = os.path.join(os.getcwd(), args.text)
     try:
         file = open(text_file_dir, "r")
-    except OSError:
+    except (FileExistsError, OSError):
         raise Exception(f"Cannot open file: {args.text}")
-    PLAY_TYPE = 2       # 열린다면 2번째 방법으로 플레이
+
+    PLAY_TYPE = 2  # 열린다면 2번째 방법으로 플레이
+
+    # 첫번째 줄의 NUM_COLS, NUM_ROWS 추출
+    tmp = file.readline()
+    tmp = tmp.split(" ")
+    NUM_ROWS, NUM_COLS = int(tmp[0]), int(tmp[1].rstrip())
+
+    # 두번째 줄의 NUM_CELLS_INIT
+    tmp = file.readline()
+    NUM_CELLS_INIT = int(tmp.rstrip())
+
+    # 세번째 줄부터 Point 객체를 만들어 initial_list에 append
+    for i in range(NUM_CELLS_INIT):
+        tmp = file.readline()
+        tmp = tmp.split(" ")
+        initial_cell_list.append(Point(int(tmp[0]), int(tmp[1].rstrip())))
 
 if args.gen:
-    print(args.gen)
     PLAY_TYPE = 3       # 세번째 입력이 있다면 3번째 방법으로 플레이
-
+    GENERATION = args.gen
 
 if __name__ == "__main__":
 
     if PLAY_TYPE == 1:
-        base.Manager()
+        Manager()
     elif PLAY_TYPE == 2:
-        pass
+        # print('PLAY_TYPE = 2')
+        Manager(list=initial_cell_list)
     elif PLAY_TYPE == 3:
-        pass
+        print('PLAY_TYPE = 3')
